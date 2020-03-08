dfcc5e261af57e65a5e73fd0c2e8cee7cbf7f87c
Juan Bertinetti <juanbertinetti@gmail.com>
2020-03-08 15:59:33 -0300

    Initial simple version

---
+++ share.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from functools import partial
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address '
+                             '[default: all interfaces]')
+    parser.add_argument('--directory', '-d', default=os.getcwd(),
+                        help='Specify alternative directory '
+                        '[default:current directory]')
+    parser.add_argument('port', action='store',
+                        default=8000, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 8000]')
+    args = parser.parse_args()
+
+    HandlerClass = partial(SimpleHTTPRequestHandler, directory=args.directory)
+    HandlerClass.protocol_version = "HTTP/1.0"
+    server_address = (args.bind, args.port)
+    with ThreadingHTTPServer(server_address, HandlerClass) as httpd:
+        sa = httpd.socket.getsockname()
+        serve_message = "Serving HTTP on {host} port {port} (http://{host}:{port}/) ..."
+        print(serve_message.format(host=sa[0], port=sa[1]))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            exit("\nKeyboard interrupt received, exiting.")
